20c93f85c930e15cc41eb7e52af459fe6c497211
JairAguirre <aguirre.jair@gmail.com>
2021-05-31 20:43:29 -0500

    some changes to terraform

--- terraformcf/variables.tf
+++ terraformcf/variables.tf
@@ -1,23 +1,30 @@
 variable "aws_region" {
   type    = string
-  default = "us-east-2" 
+  default = "us-east-2"
 }
 
 variable "automation_vpc_id" {
   type    = string
-  default = "vpc-025ce59ecf7aed804"
-}
-
-variable "public_subnet_id_2c" {
-  type    = string
-  default = "subnet-0c4d638d6183bde99"
+  default = "vpc-00537cce805fc40d5"
 }
 
 variable "public_subnet_id_2a" {
   type    = string
-  default = "subnet-02018063ae6cb01aa"
+  default = "subnet-0578b7c357c451a05"
 }
 
+variable "private_subnet_id_2a" {
+  type    = string
+  default = "subnet-072e50c4bdd28c0fc"
+}
+
+variable "private_subnet_id_2c" {
+  type    = string
+  default = "subnet-0a6d0cefe43198a73"
+}
+
+
+##aquí son cosas que no tengo idea. 
 variable "ami_id" {
   type    = string
   default = "ami-08962a4068733a2b6"
